b5a89bedcb195e03bd43d17c19d5d4c799eb1460
Pierre Marsais <pierre.marsais@lse.epita.fr>
2026-04-25 04:28:01 +0200

    Add coredump for ptrace mode.

--- kine.c
+++ kine.c
@@ -407,133 +407,12 @@ static int arch_prctl(int op, unsigned long* addr) {
 }
 #endif
 
-static void k_prepare(void) {
-#if __x86_64__
-	if (arch_prctl(ARCH_GET_FS, &k_thread_fs) < 0)
-		err(1, "arch_prctl(ARCH_GET_FS)");
-#endif
+static void *memrchr_inv(const void *s, int c, size_t n) {
+	const char *ptr = s;
+	while (n && ptr[n - 1] == c)
+		--n;
 
-	set_ldt_entry(SEGMENT_CODE, 2, 1);
-	set_ldt_entry(SEGMENT_DATA, 0, 0);
-
-	k_state.starttime = getms();
-}
-
-static int k_thread_syscall_user_dispatch_ex(entry_t entry, int probe) {
-	if (set_syscall_user_dispatch((char*)config.base + config.limit,
-				      (void*)~(0x1ULL<<63)) < 0) {
-		if (probe)
-			return -1;
-		err(1, "set_syscall_user_dispatch");
-	}
-
-	k_setup_sighandler();
-
-	k_prepare();
-	k_start(entry);
-}
-
-static void* k_thread_syscall_user_dispatch(void* entry) {
-	k_thread_syscall_user_dispatch_ex(entry, 0);
-	errx(1, "k_thread_syscall_user_dispatch_ex");
-}
-
-static int ptrace_interrupted;
-
-static void ptrace_sa_handler(int signum) {
-	ptrace_interrupted = signum;
-}
-
-static void set_interrupt_sighandlers(void (*handler)(int)) {
-	const int sigs[] = { SIGTERM, SIGHUP, SIGINT, SIGQUIT, SIGPIPE, };
-
-	for (size_t i = 0; i < ARRSZE(sigs); i++) {
-		if (sigaction(sigs[i], &(struct sigaction) {
-				.sa_handler = handler,
-			      }, NULL) < 0)
-			err(1, "sigaction");
-	}
-}
-
-__attribute((noreturn))
-static void ptrace_interrupted_reraise(void) {
-	set_interrupt_sighandlers(SIG_DFL);
-
-	raise(ptrace_interrupted);
-
-	errx(1, "ptrace_interrupted_reraise");
-}
-
-static void* k_thread_ptrace(void* entry) {
-	k_prepare();
-
-	pid_t pid = fork();
-	if (!pid) {
-		if (ptrace(PTRACE_TRACEME) < 0)
-			err(1, "ptrace(PTRACE_TRACEME)");
-
-		if (raise(SIGSTOP) < 0)
-			err(1, "raise(SIGSTOP)");
-
-		k_start(entry);
-	}
-
-	set_interrupt_sighandlers(ptrace_sa_handler);
-
-	int wstatus;
-	while (waitpid(pid, &wstatus, __WALL) >= 0) {
-		if (ptrace_interrupted) {
-			if (kill(pid, 9) < 0)
-				err(1, "kill");
-			ptrace_interrupted_reraise();
-		}
-
-		if (!WIFSTOPPED(wstatus)) {
-			warnx("waitpid: unsupported");
-			continue;
-		}
-
-		if (!(WSTOPSIG(wstatus) & 0x80)) {
-			/* 0x80 not set: We are not handling a syscall. */
-
-			if (ptrace(PTRACE_SETOPTIONS, pid, NULL,
-				   PTRACE_O_TRACESYSGOOD) < 0)
-				err(1, "ptrace(SETOPTIONS)");
-			goto next;
-		}
-
-		struct user_regs_struct regs = {};
-		if (ptrace(PTRACE_GETREGS, pid, NULL, &regs) < 0)
-			err(1, "ptrace(GETREGS)");
-
-#ifdef __x86_64__
-#define UREG(X) regs.r##X
-#define rorig_ax orig_rax
-#else
-#define UREG(X) regs.e##X
-#define eorig_ax orig_eax
-#endif
-		UREG(ax) = syscall_dispatch(UREG(orig_ax),
-					    (syscall_args_t) {
-						UREG(bx),
-						UREG(cx),
-						UREG(dx),
-					    });
-
-		if (ptrace(PTRACE_SETREGS, pid, NULL, &regs) < 0)
-			err(1, "ptrace(SETREGS)");
-
-next:
-		if (ptrace(PTRACE_SYSEMU, pid, 0, 0) < 0)
-			err(1, "ptrace(SYSEMU)");
-	}
-	err(1, "waitpid");
-}
-
-static void* k_thread_auto(void* entry) {
-	if (k_thread_syscall_user_dispatch_ex(entry, 1) < 0)
-		return k_thread_ptrace(entry);
-	return NULL;
+	return (void *)&ptr[n];
 }
 
 static const char* coredump_name(void) {
@@ -542,22 +421,6 @@ static const char* coredump_name(void) {
 	snprintf(out, sizeof(out), "kine.%d.dump", getpid());
 
 	return out;
-}
-
-static uint16_t get_cs(const ucontext_t* ctx) {
-#if __x86_64__
-	return ctx->uc_mcontext.gregs[REG_CSGSFS] & 0xffff;
-#else
-	return ctx->uc_mcontext.gregs[REG_CS];
-#endif
-}
-
-static void *memrchr_inv(const void *s, int c, size_t n) {
-	const char *ptr = s;
-	while (n && ptr[n - 1] == c)
-		--n;
-
-	return (void *)&ptr[n];
 }
 
 static void coredump_write(const struct user_regs_struct_i386 *regs) {
@@ -644,6 +507,168 @@ static void coredump_write(const struct user_regs_struct_i386 *regs) {
 		err(1, "write");
 
 	close(fd);
+}
+
+static void k_prepare(void) {
+#if __x86_64__
+	if (arch_prctl(ARCH_GET_FS, &k_thread_fs) < 0)
+		err(1, "arch_prctl(ARCH_GET_FS)");
+#endif
+
+	set_ldt_entry(SEGMENT_CODE, 2, 1);
+	set_ldt_entry(SEGMENT_DATA, 0, 0);
+
+	k_state.starttime = getms();
+}
+
+static int k_thread_syscall_user_dispatch_ex(entry_t entry, int probe) {
+	if (set_syscall_user_dispatch((char*)config.base + config.limit,
+				      (void*)~(0x1ULL<<63)) < 0) {
+		if (probe)
+			return -1;
+		err(1, "set_syscall_user_dispatch");
+	}
+
+	k_setup_sighandler();
+
+	k_prepare();
+	k_start(entry);
+}
+
+static void* k_thread_syscall_user_dispatch(void* entry) {
+	k_thread_syscall_user_dispatch_ex(entry, 0);
+	errx(1, "k_thread_syscall_user_dispatch_ex");
+}
+
+static int ptrace_interrupted;
+
+static void ptrace_sa_handler(int signum) {
+	ptrace_interrupted = signum;
+}
+
+static void set_interrupt_sighandlers(void (*handler)(int)) {
+	const int sigs[] = { SIGTERM, SIGHUP, SIGINT, SIGQUIT, SIGPIPE, };
+
+	for (size_t i = 0; i < ARRSZE(sigs); i++) {
+		if (sigaction(sigs[i], &(struct sigaction) {
+				.sa_handler = handler,
+			      }, NULL) < 0)
+			err(1, "sigaction");
+	}
+}
+
+__attribute((noreturn))
+static void ptrace_interrupted_reraise(void) {
+	set_interrupt_sighandlers(SIG_DFL);
+
+	raise(ptrace_interrupted);
+
+	errx(1, "ptrace_interrupted_reraise");
+}
+
+static void* k_thread_ptrace(void* entry) {
+	k_prepare();
+
+	pid_t pid = fork();
+	if (!pid) {
+		if (ptrace(PTRACE_TRACEME) < 0)
+			err(1, "ptrace(PTRACE_TRACEME)");
+
+		if (raise(SIGSTOP) < 0)
+			err(1, "raise(SIGSTOP)");
+
+		k_start(entry);
+	}
+
+	set_interrupt_sighandlers(ptrace_sa_handler);
+
+	int wstatus;
+	while (waitpid(pid, &wstatus, __WALL) >= 0) {
+		if (ptrace_interrupted) {
+			if (kill(pid, 9) < 0)
+				err(1, "kill");
+			ptrace_interrupted_reraise();
+		}
+
+		if (!WIFSTOPPED(wstatus)) {
+			warnx("waitpid: unsupported");
+			continue;
+		}
+
+		if (WSTOPSIG(wstatus) == SIGSTOP) {
+			/* 0x80 not set: We are not handling a syscall. */
+
+			if (ptrace(PTRACE_SETOPTIONS, pid, NULL,
+				   PTRACE_O_TRACESYSGOOD) < 0)
+				err(1, "ptrace(SETOPTIONS)");
+		} else if (WSTOPSIG(wstatus) == (SIGTRAP|0x80)) {
+			struct user_regs_struct regs = {};
+			if (ptrace(PTRACE_GETREGS, pid, NULL, &regs) < 0)
+				err(1, "ptrace(GETREGS)");
+
+#ifdef __x86_64__
+#define UREG(X) regs.r##X
+#define rorig_ax orig_rax
+#else
+#define UREG(X) regs.e##X
+#define eorig_ax orig_eax
+#endif
+			UREG(ax) = syscall_dispatch(UREG(orig_ax),
+						    (syscall_args_t) {
+							UREG(bx),
+							UREG(cx),
+							UREG(dx),
+						    });
+
+			if (ptrace(PTRACE_SETREGS, pid, NULL, &regs) < 0)
+				err(1, "ptrace(SETREGS)");
+		} else if (WSTOPSIG(wstatus) == SIGBUS ||
+			   WSTOPSIG(wstatus) == SIGFPE ||
+			   WSTOPSIG(wstatus) == SIGILL ||
+			   WSTOPSIG(wstatus) == SIGSEGV ||
+			   WSTOPSIG(wstatus) == SIGTRAP) {
+			struct user_regs_struct regs = {};
+			if (ptrace(PTRACE_GETREGS, pid, NULL, &regs) < 0)
+				err(1, "ptrace(GETREGS)");
+
+			coredump_write(&(struct user_regs_struct_i386) {
+					.ebx = UREG(bx),
+					.ecx = UREG(cx),
+					.edx = UREG(dx),
+					.esi = UREG(si),
+					.edi = UREG(di),
+					.ebp = UREG(bp),
+					.eax = UREG(ax),
+					.eip = UREG(ip),
+					.esp = UREG(sp),
+					.eflags = regs.eflags,
+					.orig_eax = UREG(orig_ax),
+				       });
+			if (kill(pid, 9) < 0)
+				err(1, "kill");
+			continue;
+		} else {
+			errx(1, "waitpid: unsupported signal");
+		}
+
+		if (ptrace(PTRACE_SYSEMU, pid, 0, 0) < 0)
+			err(1, "ptrace(SYSEMU)");
+	}
+	err(1, "waitpid");
+}
+
+static void* k_thread_auto(void* entry) {
+	if (k_thread_syscall_user_dispatch_ex(entry, 1) < 0)
+		return k_thread_ptrace(entry);
+	return NULL;
+}
+
+static uint16_t get_cs(const ucontext_t* ctx) {
+#if __x86_64__
+	return ctx->uc_mcontext.gregs[REG_CSGSFS] & 0xffff;
+#else
+	return ctx->uc_mcontext.gregs[REG_CS];
+#endif
 }
 
 __attribute__ ((used))
